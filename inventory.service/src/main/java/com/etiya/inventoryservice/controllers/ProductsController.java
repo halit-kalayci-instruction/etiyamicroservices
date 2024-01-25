758cb0ac7fb9f425cfa2ef2cd5afc050f3b91d6a
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2024-01-25 11:00:05 +0300

    .

---
+++ inventory.service/src/main/java/com/etiya/inventoryservice/controllers/ProductsController.java
@@ -0,0 +1,13 @@
+package com.etiya.inventoryservice.controllers;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/products")
+public class ProductsController {
+
+    @GetMapping()
+    public boolean checkStock(@RequestParam int productId) {
+        return true;
+    }
+}
